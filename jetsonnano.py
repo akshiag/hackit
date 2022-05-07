f034613263f8a3c872ba3310afb2198ab3379303
akshiag <akshiag@users.noreply.github.com>
2022-05-07 03:08:20 +0200

    Create jetsonnano.py

---
+++ jetsonnano.py
@@ -0,0 +1,65 @@
+import cv2
+from pyzbar import pyzbar
+
+
+def gstreamer_pipeline(
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink drop=True"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+def read_barcodes(frame):
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        x, y, w, h = barcode.rect
+        # 1
+        barcode_info = barcode.data.decode('utf-8')
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # 2
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
+        # 3
+        with open("barcode_result.txt", mode='w') as file:
+            file.write("Recognized Barcode:" + barcode_info)
+    return frame
+
+
+def main():
+    #1
+    camera = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+    ret, frame = camera.read()
+    #2
+    while ret:
+        ret, frame = camera.read()
+        frame = read_barcodes(frame)
+        cv2.imshow('Barcode/QR code reader', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    #3
+    camera.release()
+    cv2.destroyAllWindows()
+#4
+if __name__ == '__main__':
+    main()
